4bab5d886e45a899f8ae689495c498386a1a6d4c
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-08-24 22:08:38 -0300

    first commit

---
+++ src/assets/components/initialContent.ts
@@ -0,0 +1,22 @@
+export const initialContent = `
+ <p>Se você é um programador, sabe o quão importante é manter tudo organizado para otimizar seu fluxo de trabalho. Uma ferramenta que tem ganhado destaque nesse aspecto é o Notion.</p>
+              
+              <h2>O Poder das Listas de Tarefas</h2>
+              <p>Uma das funcionalidades mais úteis do Notion é a capacidade de criar listas de tarefas. Na programação, isso pode ser aproveitado para criar listas de afazeres específicas para cada projeto. Marque as tarefas à medida que você as conclui e mantenha-se no caminho certo.</p>
+
+              <h2>Gerenciamento de Projetos Simplificado</h2>
+              <p>O Notion permite que você crie bancos de dados personalizados. Isso é especialmente útil para gerenciar projetos. Você pode criar uma base de dados para acompanhar problemas, recursos, requisitos e muito mais. Isso mantém todas as informações essenciais em um só lugar.</p>
+              
+              <blockquote>
+                  <p>A capacidade de personalização do Notion é incrível. Criei um banco de dados para monitorar o progresso de cada tarefa do meu projeto, o que me ajudou a manter a equipe alinhada." - Desenvolvedor Satisfeito</p>
+              </blockquote>
+
+              <h2>Integração com Ferramentas de Desenvolvimento</h2>
+              <p>Além de suas funcionalidades internas, o Notion oferece integrações com ferramentas de desenvolvimento populares. Você pode incorporar trechos de código diretamente em suas páginas, vincular repositórios do GitHub e muito mais.</p>
+              
+              <h3>Exemplo de Incorporação de Código:</h3>
+                <pre>
+                  <code class="language-html">var teste = 'aloo';</code>
+                </pre>
+
+`
